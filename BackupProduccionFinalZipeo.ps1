2864f87fb3175ead82ffdf320aae4e8881583c4b
TorassaValentin2am <valentin.torassacolombero@alumnos.uai.edu.ar>
2025-01-03 14:16:07 -0300

    mejor comprension del texto de consola

--- BackupProduccionFinalZipeo.ps1
+++ BackupProduccionFinalZipeo.ps1
@@ -26,30 +26,69 @@ $BackupName = "Backup-$Now"
 $DestinationFolder = Join-Path $BackupPath $BackupName
 New-Item -ItemType Directory -Path $DestinationFolder | Out-Null
 
+Write-Host "                                      "
 Write-Host "Iniciando respaldo de Hyper-V --> $DestinationFolder ..."
 robocopy "D:\Youtube" $DestinationFolder /E /R:3 /W:5
-Write-Host "Copia finalizada. Se guardó en: $DestinationFolder"
 
+Write-Host "--------------------------------------"
+Write-Host "                                      "
+Write-Host "Copia finalizada. Se guardó en: $DestinationFolder"
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 
 $ZipFileName = "$BackupName.zip"
 $ZipFilePath = Join-Path $BackupPath $ZipFileName
 
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "Comprimiendo carpeta en: $ZipFilePath ..."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
+
+
 Compress-Archive -Path $DestinationFolder -DestinationPath $ZipFilePath
+
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "Carpeta comprimida correctamente."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "Eliminando carpeta sin comprimir: $DestinationFolder ..."
-Remove-Item -Recurse -Force $DestinationFolder
-Write-Host "Carpeta sin comprimir eliminada."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 
+
+Remove-Item -Recurse -Force $DestinationFolder
+
+Write-Host "--------------------------------------"
+Write-Host "                                      "
+Write-Host "Carpeta sin comprimir eliminada."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
+
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "Eliminando respaldos .zip con mas de 8 dias..."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 
 Get-ChildItem -Path $BackupPath -Filter '*.zip' | Where-Object {
     $_.CreationTime -lt (Get-Date).AddDays(-8)
 } | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "Copias antiguas (mas de 8 dias) eliminadas (si existian)."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 
 
+Write-Host "--------------------------------------"
+Write-Host "                                      "
 Write-Host "La tarea ha finalizado."
+Write-Host "                                      "
+Write-Host "--------------------------------------"
 Start-Sleep -Seconds 120
